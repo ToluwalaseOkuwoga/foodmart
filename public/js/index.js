d28380e230c6eb0ccfd320551f924fd9b26128d5
ToluwalaseOkuwoga <toluwalaseokuwoga@yahoo.com>
2019-09-19 12:09:34 +0100

    user and admin login js for db

---
+++ public/js/index.js
@@ -0,0 +1,55 @@
+$(function() {
+
+    $('#id01').submit((e) => {
+      e.preventDefault();
+  const email = document.querySelector(".email").value;
+  const password = document.querySelector(".password").value;
+  
+  $.ajax(
+    {
+       type:'GET',
+       url: 'http://localhost:3000/admin',
+       success:function(data){
+         console.log(data);
+         $.each(data, function(index,admin){
+           if(user.email === email && user.password === password){
+             window.location.href = "../dashboard"
+           }else{
+             console.log('error')
+           }
+         })
+       },
+       error:function(){
+          console.log("no db");
+       }
+    }
+  );
+  })
+
+  $('#id02').submit((e) => {
+    e.preventDefault();
+const email = document.querySelector(".email").value;
+const password = document.querySelector(".password").value;
+
+$.ajax(
+  {
+     type:'GET',
+     url: 'http://localhost:3000/user',
+     success:function(data){
+       console.log(data);
+       $.each(data, function(index,admin){
+         if(admin.email === email && admin.password === password){
+           window.location.href = "../dashboard"
+         }else{
+           console.log('error')
+         }
+       })
+     },
+     error:function(){
+        console.log("no db");
+     }
+  }
+);
+})
+  })
+  
